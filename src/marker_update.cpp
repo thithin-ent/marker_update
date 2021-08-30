54e6227ab7d45e35898ea0b3d8f6495eb0a971eb
Refstop <bhbhchoi@yu.ac.kr>
2021-08-30 13:57:32 +0900

    launch file add

--- src/marker_update.cpp
+++ src/marker_update.cpp
@@ -8,7 +8,7 @@ marker_update::marker_update()
   P_ << MatrixXd::Zero(3, 3), MatrixXd::Zero(3, randmark_N * 3),
       MatrixXd::Zero(randmark_N * 3, 3), 50000 * MatrixXd::Identity(randmark_N * 3, randmark_N * 3);
   Q_.resize(3, 3);
-  Q_ << 1, 0, 0, 0, 1, 0, 0, 0, 1;
+  Q_ << 0.1, 0, 0, 0, 0.1, 0, 0, 0, 0.1;
   T_B_C << 0, 0, 1, -1, 0, 0, 0, -1, 0;
   std::cout << "set_sstart" << std::endl;
   pose_sub = nh.subscribe("pose", 1, &marker_update::posecallback, this);
@@ -17,10 +17,10 @@ marker_update::marker_update()
 
 void marker_update::correction(const VectorXd &Z, const MatrixXd &H, const VectorXd &z_hat)
 {
-  //std::cout << "----X_--------" << std::endl;
-  //std::cout << X_ << std::endl;
-  //std::cout << "----P_--------" << std::endl;
-  //std::cout << P_ << std::endl;
+  std::cout << "----X_--------" << std::endl;
+  std::cout << X_ << std::endl;
+  std::cout << "----P_--------" << std::endl;
+  std::cout << P_ << std::endl;
   MatrixXd K(3 + randmark_N * 3, 3);
   K = P_ * H.transpose() * (H * P_ * H.transpose() + Q_).inverse();
   X_ = X_ + K * (Z - z_hat);
@@ -48,7 +48,6 @@ void marker_update::markercallback(const fiducial_msgs::FiducialTransformArray::
 
     double roll, pitch, yaw;
     tf::Matrix3x3(T_quaternion).getRPY(roll, pitch, yaw);
-    std::cout << pitch<< std::endl;
     Z << data->transforms[i].transform.translation.z, -data->transforms[i].transform.translation.x, -pitch;
 
     MatrixXd low_H(3, 6);
@@ -60,13 +59,13 @@ void marker_update::markercallback(const fiducial_msgs::FiducialTransformArray::
     F << MatrixXd::Identity(3, 3), MatrixXd::Zero(3, randmark_N * 3),
         MatrixXd::Zero(3, 3 + marker_id * 3), MatrixXd::Identity(3, 3), MatrixXd::Zero(3, 3 * randmark_N - 3 * marker_id - 3);
 
-    //std::cout << "----z_hat-------" << std::endl;
+    std::cout << "----z_hat-------" << std::endl;
 
     Vector3d z_hat;
     z_hat << cos(X_(2)) * X_(3 + marker_id * 3) + sin(X_(2)) * X_(4 + marker_id * 3) - cos(X_(2)) * X_(0) - sin(X_(2)) * X_(1),
         -sin(X_(2)) * X_(3 + marker_id * 3) + cos(X_(2)) * X_(4 + marker_id * 3) + sin(X_(2)) * X_(0) - cos(X_(2)) * X_(1),
         X_(5 + marker_id * 3) - X_(2);
-    //std::cout << z_hat << std::endl;
+    std::cout << z_hat << std::endl;
 
     correction(Z, low_H * F, z_hat);
   }
